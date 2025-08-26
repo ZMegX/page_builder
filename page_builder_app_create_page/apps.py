b48b2a6c8e0f3811c4d7e910969ab8e82cf120ba
ZMegX <picardomegan@gmail.com>
2025-08-26 20:20:05 +0100

    rename the app

--- page_builder_app_create_page/apps.py
+++ page_builder_app_create_page/apps.py
@@ -1,6 +1,4 @@
 from django.apps import AppConfig
 
-
-class FormsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'forms'
+class PageBuilderAppCreatePageConfig(AppConfig):
+    name = 'page_builder_app_create_page'
