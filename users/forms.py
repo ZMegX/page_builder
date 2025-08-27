c4bdacf8a7701fceff8b5a8b4ce20f193ee6d550
ZMegX <picardomegan@gmail.com>
2025-08-27 16:28:23 +0100

    merged profile form and profileupdate form as the same

--- users/forms.py
+++ users/forms.py
@@ -1,68 +1,16 @@
 from django import forms
-from .models import Profile, Address
-from django.contrib.auth.forms import UserCreationForm, SetPasswordForm #Inheritance Relationship
+from django.contrib.auth.forms import UserCreationForm, SetPasswordForm
 from django.contrib.auth.models import User
+from .models import Profile, Address
 
+# Profile Create/Edit Form
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = [
-            'company_name',
-            'image',
-            'email',
-            'phone_number',
-            'description',
-            'cuisine_type',
-            'established_year',
-            'registration_number',
-            'tax_id',
-            'website',
-            'capacity',
-        ]
-        widgets = {
-            'description': forms.Textarea(attrs={'rows': 3}),
-        }
-
-class AddressForm(forms.ModelForm):
-    class Meta:
-        model = Address
-        fields = [
-            'street',
-            'city',
-            'state',
-            'country',
-            'zipcode',
-            'latitude',
-            'longitude',
-        ]
-
-
-
-class CustomUserCreationForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
-        fields = UserCreationForm.Meta.fields + ("email",)
-        
-class UserRegisterForm(UserCreationForm):
-  email = forms.EmailField()
-
-  class Meta:
-    model = User
-    fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
-
-class UserUpdateForm(forms.ModelForm):
-    email = forms.EmailField()
-
-    class Meta:
-        model = User
-        fields = ['username', 'first_name', 'last_name', 'email']
-
-class ProfileUpdateForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        fields = [
-            'company_name', 'image', 'phone_number', 'description',
+            'company_name', 'image', 'email', 'phone_number', 'description',
             'cuisine_type', 'established_year', 'registration_number',
-            'tax_id', 'website', 'capacity'
+            'tax_id', 'website', 'capacity',
         ]
         widgets = {
             'description': forms.Textarea(attrs={'rows': 3}),
@@ -71,13 +19,40 @@ class ProfileUpdateForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        # Make image optional
         self.fields['image'].required = False
 
+# Profile Update Form (same as ProfileForm, can be omitted if not differing)
+ProfileUpdateForm = ProfileForm
 
+# Address Create/Edit Form
+class AddressForm(forms.ModelForm):
+    class Meta:
+        model = Address
+        fields = [
+            'street', 'city', 'state', 'country', 'zipcode',
+            'latitude', 'longitude',
+        ]
 
+# User Registration Form (extend as needed)
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = UserCreationForm.Meta.fields + ("email",)
+
+# User Update Form
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email']
+
+# Custom Set Password Form (for password reset/change flows)
 class CustomSetPasswordForm(SetPasswordForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # Add any custom attributes here, e.g., placeholders
         for field in self.fields.values():
             field.widget.attrs.update({'class': 'form-control'})
