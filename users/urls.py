d647c0ed3232d43d1458b3f6e07728872dfc4dfa
ZMegX <picardomegan@gmail.com>
2025-08-27 16:29:02 +0100

    register and profile_manage urls

--- users/urls.py
+++ users/urls.py
@@ -2,9 +2,7 @@ from django.urls import path, include
 from users import views
 
 urlpatterns = [
-    path('complete_profile/', views.complete_profile, name='complete_profile'),
-    path("accounts/", include("django.contrib.auth.urls")),
-    path("register/", views.create_profile, name="registration"),
-    path('profile/', views.profile_view, name='profile'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile_manage, name='profile'),
 
 ]
