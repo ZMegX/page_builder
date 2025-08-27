aabf4bac85d50da4fcd2fdea12fa35bff83ea99d
ZMegX <picardomegan@gmail.com>
2025-08-27 18:22:30 +0100

    restaurantdetails view added in manage profile

--- users/views.py
+++ users/views.py
@@ -1,13 +1,13 @@
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from .forms import (
                     AddressForm, 
                     CustomUserCreationForm, 
                     UserUpdateForm, 
-                    ProfileUpdateForm
+                    ProfileUpdateForm,
+                    RestaurantDetailsForm
                     )
-from .models import Profile
+from .models import Profile, RestaurantDetails
 
 def register(request):
     if request.method == 'POST':
@@ -20,31 +20,38 @@ def register(request):
         form = CustomUserCreationForm()
     return render(request, 'registration/register.html', {'form': form})
 
-# Profile creation/editing view (NO password here)
 @login_required
 def profile_manage(request):
     user = request.user
     profile, created = Profile.objects.get_or_create(user=user)
+    restaurant_details, _ = RestaurantDetails.objects.get_or_create(profile=profile)
 
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, instance=user)
-        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=profile)
         address_form = AddressForm(request.POST)
+        # get or create restaurant details instance for the profile
+        r_form = RestaurantDetailsForm(request.POST, instance=restaurant_details)
+
         if u_form.is_valid() and p_form.is_valid() and address_form.is_valid():
             u_form.save()
             p_form.save()
-            address = AddressForm.save()
+            address = address_form.save()
             profile.addresses.add(address)
-            return redirect('profile')  # Redirect to profile page after completion
+            r_form.save()
+            return redirect('profile')
     else:
-        u_form= UserUpdateForm(instance=user)
+        u_form = UserUpdateForm(instance=user)
         p_form = ProfileUpdateForm(instance=profile)
         address_form = AddressForm()
+        r_form = RestaurantDetailsForm(instance=restaurant_details)
 
     return render(request, 'users/profile.html', {
         'u_form': u_form,
         'p_form': p_form,
         'address_form': address_form,
         'profile': profile,
+        'r_form': r_form,
+
     })
 
