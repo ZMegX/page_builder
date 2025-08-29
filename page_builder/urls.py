3206ddc551f5db3dc09b718bb3297c0feb791cb4
ZMegX <picardomegan@gmail.com>
2025-08-29 01:15:26 +0100

    page_builder v1

--- page_builder/urls.py
+++ page_builder/urls.py
@@ -17,6 +17,8 @@ urlpatterns = [
     # Django's built-in authentication URLs (login, logout, password management)
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('users.urls')),
+    path('builder/', include('page_builder_app_create_page.urls')),  # This makes your dashboard app the root page
+
 
 ]
 
